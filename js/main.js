51803156a0d0de8eaca9b56360bee40c621f3202
sametj <mosesjames2000@gmail.com>
2024-01-15 19:15:15 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,297 @@
+const API_BASE_URL = "http://localhost:3000/";
+const API_URL = API_BASE_URL + "todos/";
+const tasksList = document.querySelector(".tasks");
+const tags = document.querySelectorAll(".tag");
+const days = document.querySelectorAll(".day");
+const addTask = document.querySelector(".add-button");
+const alert = document.querySelector(".alert-section");
+const dateElement = document.querySelector("#date");
+const activeDay = document.querySelector(".active");
+const activeTag = document.querySelector(".selected");
+const deleteButtons = document.querySelectorAll(".delete-button");
+const editButtons = document.querySelectorAll(".edit-button");
+const saveButtons = document.querySelectorAll(".save-button");
+const cancelButtons = document.querySelectorAll(".cancel-button");
+
+//displaying date
+const date = new Date();
+const format = {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+  year: "numeric",
+};
+const today = date.toLocaleDateString("en-US", format);
+dateElement.innerHTML = today;
+
+//displaying tasks
+const getTodo = (todo) => {
+  let html = "";
+  todo.forEach((element) => {
+    html += `  <li class="task" id ="${element.tag}">
+          <span class ="uniqueId">${element.id}</span>
+          <div class="task-div">${element.task} </div>
+          <div class="buttons-div">
+            <button class="edit-button">Edit</button>
+            <button class="save-button">Save</button>
+            <button class="cancel-button">Cancel</button>
+            <button class="delete-button">Delete</button>
+          </div>
+          <input type="checkbox" class="checkbox" ${
+            element.completed === true ? "checked" : ""
+          } />
+        </li>`;
+    tasksList.innerHTML = html;
+  });
+};
+
+//selecting day
+days.forEach((day) => {
+  day.addEventListener("click", () => {
+    days.forEach((day) => {
+      tasksList.innerHTML = "";
+      day.classList.remove("active");
+    });
+    day.classList.add("active");
+    fetch(API_URL + `${day.id}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        getTodo(data);
+        window.location.href = `#${day.id}`;
+      });
+  });
+});
+
+//selecting tag
+tags.forEach((tag) => {
+  tag.addEventListener("click", () => {
+    if (tag.classList.contains("selected")) {
+      tag.classList.remove("selected");
+    } else {
+      tags.forEach((tag) => {
+        tag.classList.remove("selected");
+      });
+      tag.classList.add("selected");
+    }
+  });
+});
+
+//adding task
+addTask.addEventListener("click", () => {
+  const taskText = document.querySelector(".task-input");
+  const selectedDay = document.querySelector(".active").id;
+  const selectedTag = document.querySelector(".selected");
+  const task = taskText.value;
+
+  if (task == "") {
+    alert.innerHTML = "Please enter a task";
+    alert.classList.add("show");
+    setTimeout(() => {
+      alert.classList.remove("show");
+    }, 3000);
+  } else if (!selectedTag) {
+    alert.innerHTML = "Please select a tag";
+    alert.classList.add("show");
+    setTimeout(() => {
+      alert.classList.remove("show");
+    }, 3000);
+  } else {
+    taskText.value = "";
+    alert.innerHTML = "Task added successfully";
+    alert.classList.add("show");
+    alert.style.backgroundColor = "green";
+    setTimeout(() => {
+      alert.classList.remove("show");
+    }, 3000);
+    const tag = selectedTag.id;
+    fetch(API_URL + "addtask", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        task: task,
+        day: selectedDay,
+        tag: tag,
+        completed: false,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const html = `  <li class="task" id ="${tag}">
+          <span class ="uniqueId">${data.id}</span>
+          <div class="task-div">${task} </div>
+          <div class="buttons-div">
+            <button class="edit-button">Edit</button>
+            <button class="save-button">Save</button>
+            <button class="cancel-button">Cancel</button>
+            <button class="delete-button">Delete</button>
+          </div>
+          <input type="checkbox" class="checkbox" />
+        </li>`;
+        tasksList.innerHTML += html;
+      });
+  }
+});
+
+//Deleting task
+document.addEventListener("click", (e) => {
+  const selectedDay = document.querySelector(".active").id;
+  if (e.target.classList.contains("delete-button")) {
+    const id = e.target.parentElement.parentElement.children[0].innerText;
+    fetch(API_URL + `delete/${id}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        id: id,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then(() => {
+        alert.classList.add("show");
+        alert.innerHTML = "Task deleted successfully";
+        setTimeout(() => {
+          alert.classList.remove("show");
+        }, 3000);
+        tasksList.removeChild(e.target.parentElement.parentElement);
+      });
+  }
+});
+
+//Editing task
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("edit-button")) {
+    const id = e.target.parentElement.parentElement.children[0].innerText;
+
+    //Getting Buttons and checkbox
+    const deleteButton = e.target.parentElement.children[3];
+    const editButton = e.target.parentElement.children[0];
+    const saveButton = e.target.parentElement.children[1];
+    const cancelButton = e.target.parentElement.children[2];
+    const checkbox = e.target.parentElement.parentElement.children[3];
+
+    //hiding buttons
+    deleteButton.style.display = "none";
+    editButton.style.display = "none";
+    saveButton.style.display = "block";
+    cancelButton.style.display = "block";
+    checkbox.classList.add("hide");
+
+    //Getting task
+    const task = e.target.parentElement.parentElement.children[1];
+    const taskText = task.innerText;
+    const taskInput = document.createElement("input");
+    taskInput.classList.add("edit-input");
+    taskInput.value = taskText;
+    task.innerText = "";
+    task.appendChild(taskInput);
+
+    //save task
+    cancelButton.addEventListener("click", () => {
+      task.innerText = taskText;
+      deleteButton.style.display = "block";
+      editButton.style.display = "block";
+      saveButton.style.display = "none";
+      cancelButton.style.display = "none";
+      checkbox.classList.remove("hide");
+    });
+
+    saveButton.addEventListener("click", () => {
+      const newTask = taskInput.value;
+      fetch(API_URL + `edit/${id}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          id: id,
+          task: newTask,
+        }),
+      }).then(() => {
+        alert.classList.add("show");
+        alert.innerHTML = "Task updated successfully";
+        setTimeout(() => {
+          alert.classList.remove("show");
+        }, 3000);
+        task.innerText = newTask;
+        deleteButton.style.display = "block";
+        editButton.style.display = "block";
+        saveButton.style.display = "none";
+        cancelButton.style.display = "none";
+        checkbox.classList.remove("hide");
+      });
+    });
+  }
+});
+
+//Completing task
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checkbox")) {
+    const id = e.target.parentElement.children[0].innerText;
+    e.target.parentElement.classList.add("completed");
+    fetch(API_URL + `edit/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        completed: e.target.checked,
+      }),
+    }).then((data) => {
+      if (e.target.checked) {
+        alert.classList.add("show");
+        alert.innerHTML = "Task completed successfully";
+        setTimeout(() => {
+          alert.classList.remove("show");
+        }, 3000);
+      }
+    });
+  }
+});
+
+//Filtering tasks by tag
+document.addEventListener("click", (e) => {
+  const selectedDay = document.querySelector(".active").id;
+  if (e.target.classList.contains("tag")) {
+    if (e.target.classList.contains("selected")) {
+      const tag = e.target.id;
+      fetch(API_URL + `${selectedDay}/${tag}`, {
+        method: "GET",
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          tasksList.innerHTML = "";
+          getTodo(data);
+        });
+    }
+  }
+  e;
+});
+
+window.onload = () => {
+  const day = today.split(",")[0].toLowerCase();
+  const defaultDay = document.querySelector(`#${day}`);
+  defaultDay.classList.add("active");
+
+  fetch(API_URL + day, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      getTodo(data);
+    });
+};
